689e1a7a0c1d4d79d13148d7beacbe18e898acf6
Jonathan Reimer <wildreim@gmail.com>
2020-11-17 18:36:27 -0600

    named requestors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 */
 
 const unary_requestor = function (unary_fx) {
-    return function (callback) {
+    return function unary_requestor (callback) {
         return function (value) {
             try {
                 callback (unary_fx (value));
@@ -16,7 +16,7 @@ const unary_requestor = function (unary_fx) {
 
 // Insert a value into an array of requestors
 const constant_requestor = function (constant_value) {
-    return function (callback) {
+    return function constant_requestor (callback) {
         return function (ignore) {
             return callback (constant_value);
         };
@@ -25,7 +25,7 @@ const constant_requestor = function (constant_value) {
 
 // Convert a promise to a requestor
 const promise_requestor = function (promise_object) {
-    return function (callback) {
+    return function promise_requestor (callback) {
         const on_err = function (err) {
             return callback(undefined, err.message);
         };
@@ -36,8 +36,21 @@ const promise_requestor = function (promise_object) {
     };
 };
 
+const functional_callback = function (on_success) {
+    return function (on_fail) {
+        return function callback (value, reason) {
+            if (value === undefined) {
+                on_fail (reason);
+            } else {
+                on_success (value);
+            }
+        };
+    };
+};
+
 export {
     unary_requestor,
     constant_requestor,
-    promise_requestor
+    promise_requestor,
+    functional_callback
 };
